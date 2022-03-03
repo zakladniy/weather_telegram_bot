515d6c65f8c703c3b436749f67c87f4b42a1bbc4
Anton Zakladniy <anton.zakladniy@biovitrum.ru>
2022-03-03 19:38:15 +0300

    Fix web-hook url

--- src/application/main.py
+++ src/application/main.py
@@ -64,11 +64,13 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook(
-        'https://weather-spb-telegram-bot.herokuapp.com/' + TOKEN)
+    updater.start_webhook(
+        listen="0.0.0.0",
+        port=int(PORT),
+        url_path=TOKEN,
+        webhook_url='https://weather-spb-telegram-bot.herokuapp.com/' + TOKEN,
+    )
+
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
