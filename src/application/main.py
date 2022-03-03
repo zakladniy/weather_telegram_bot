3286b257041e44cb79026594015035ec08748598
Anton Zakladniy <anton.zakladniy@biovitrum.ru>
2022-03-03 19:15:03 +0300

    Update token

--- src/application/main.py
+++ src/application/main.py
@@ -17,7 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'YOURTELEGRAMBOTTOKEN'
+TOKEN = os.environ["TOKEN"]
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
