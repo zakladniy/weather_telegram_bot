450cebec8a32ca1c10fbeb1c7b16db97358e16c0
Anton Zakladniy <anton.zakladniy@biovitrum.ru>
2022-03-11 23:45:35 +0300

    Add docstrings to bot

--- src/application/main.py
+++ src/application/main.py
@@ -2,7 +2,10 @@
 import logging
 import os
 
+from telegram import Update
+from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
 from weather_utils import create_weather_message
 
 PORT = int(os.environ.get('PORT', 8443))
@@ -16,24 +19,40 @@ logger = logging.getLogger(__name__)
 TOKEN = os.environ["TOKEN"]
 
 
-def start(update, context) -> None:
-    """Send a message when the command /start is issued."""
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued.
+
+    @param update: incoming update
+    @param context: context object
+    """
     weather = create_weather_message()
     update.message.reply_text(weather)
 
 
-def help_command(update, context) -> None:
-    """Send a message when the command /help is issued."""
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued.
+
+    @param update: incoming update
+    @param context: context object
+    """
     update.message.reply_text('Help!')
 
 
-def echo(update, context) -> None:
-    """Echo the user message."""
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message.
+
+    @param update: incoming update
+    @param context: context object
+    """
     update.message.reply_text(update.message.text)
 
 
-def error(update, context) -> None:
-    """Log Errors caused by Updates."""
+def error(update: Update, context: CallbackContext) -> None:
+    """Log errors caused by Updates.
+
+    @param update: incoming update
+    @param context: context object
+    """
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
@@ -57,7 +76,7 @@ def main() -> None:
         listen="0.0.0.0",
         port=int(PORT),
         url_path=TOKEN,
-        webhook_url='https://weather-spb-telegram-bot.herokuapp.com/' + TOKEN,
+        webhook_url=f'https://weather-spb-telegram-bot.herokuapp.com/{TOKEN}',
     )
 
     # Run the bot
